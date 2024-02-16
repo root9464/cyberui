b76cb0d4bfba9b4a3c631800e46ec9af4527140f
root9464 <senpai9464@yandex.ru>
2024-02-16 23:14:08 +0500

    -

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -1,7 +1,8 @@
 import {Button} from '@/components/Button/button.ui';
+import styles from './page.module.sass';
 export default function Buttons() {
 	return (
-		<div>
+		<div className={styles.div}>
 			<p>buttons</p>
 			<Button theme='light' variant='default' text='Primary' />
 
