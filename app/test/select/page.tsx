6a87e22b948c68833b320a3a15eb5ebad4ba7fd2
root9464 <senpai9464@yandex.ru>
2024-03-07 18:36:13 +0500

    07.03.2024 | 18:35 || "Полностью готовый компонент selects"

--- app/test/select/page.tsx
+++ app/test/select/page.tsx
@@ -1,12 +1,28 @@
 import {Select} from '@/components/Select/select.ui';
 import styles from './page.module.sass';
+import type {Metadata} from 'next';
+
+export const metadata: Metadata = {
+	title: 'CyberUI - Selects',
+};
 export default function SelectPage() {
 	const Content: string[] = [
 		'Home', 'About', 'Services', 'Blog', 'Contact',
 	];
 	return (
 		<div className={styles.div}>
-			<Select {...{content: Content, theme: 'dark', variant: 'default'}}/>
+			<p>selects</p>
+			<div className={styles.box}>
+				<div className={styles.selects}>
+					<p>Light Theme</p>
+					<Select {...{content: Content, theme: 'light', variant: 'default'}} />
+					<Select {...{content: Content, theme: 'light', variant: 'default'}}/>
+				</div>
+				<div className={styles.selects}>
+					<p>Dark Theme</p>
+					<Select {...{content: Content, theme: 'dark', variant: 'deep'}}/>
+				</div>
+			</div>
 		</div>
 	);
 }
---
+++ app/test/test/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.sass';
+export default function ButtonsPage() {
+	return (
+		<div className={styles.div}>
+			<p>buttons</p>
+
+		</div>
+	);
+}
